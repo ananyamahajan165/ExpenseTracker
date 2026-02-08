c4f490493e1d617e3b68bd9dd0987c91b4539f01
Ananya Mahajan <ananyam054@gmail.com>
2026-02-08 16:03:46 +0530

    added a new file

---
+++ TestDB.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+
+public class TestDB {
+    public static void main(String[] args) {
+        Connection con = DBConnection.getConnection();
+        if (con != null) {
+            System.out.println("✅ Connected to MySQL successfully!");
+        } else {
+            System.out.println("❌ Connection failed");
+        }
+    }
+}
